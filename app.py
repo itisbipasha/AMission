9be77d13e2e95af6e6094dcb0c2a48b4abdda75c
Bipasha <bipasharouth05@gmail.com>
2026-06-22 13:01:32 +0530

    first commit

--- app.py
+++ app.py
@@ -227,4 +227,5 @@ def due_soon():
     return jsonify(due)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port= int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0',port=port,debug=False)
